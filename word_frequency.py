a8134230663d8f4bd40913ddb3edf5c305c0acb6
TrentHurlbut <trent.hurlbut@gmail.com>
2021-10-26 12:00:55 -0700

    basic functionality, need final formatting

--- word_frequency.py
+++ word_frequency.py
@@ -4,16 +4,40 @@ STOP_WORDS = [
     'will', 'with'
 ]
 
+import string
+import operator
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
+    with open(file) as text:
+        content = text.read()
+        formatted_content = content.replace(string.punctuation, "")
+        cleaned_content = formatted_content.replace("-" , " ").replace("—", " ").replace(".", "").replace(",", "").replace(":", "").replace("'", "").replace('"', "").replace("-\n","").lower().split()
+
+    keys = {}
+    for word in cleaned_content:
+        if word in STOP_WORDS:
+            pass
+        elif word not in keys:
+            keys[word] = 1
+        else:
+            keys[word] += 1
+
+    asterisk = '*'
+    i = 0
+
+    while i < 10:
+        max_key = max(keys, key=keys.get)
+        print(f"{max_key} | {keys[max_key]} {asterisk * keys[max_key]}")
+        keys.pop(max_key)
+        i += 1
+
     pass
 
 
 if __name__ == "__main__":
     import argparse
     from pathlib import Path
-
     parser = argparse.ArgumentParser(
         description='Get the word frequency in a text file.')
     parser.add_argument('file', help='file to read')
